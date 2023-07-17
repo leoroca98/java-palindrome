128cd1e1ff8c2c06c73f6f062b0cb7161edb78cb
leoroca98 <leoroca12345@gmail.com>
2023-07-17 15:42:43 +0200

    frist commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+        boolean flag=true;
+        //chiedere all'utente di inserire la prima parola
+        System.out.print("inserisci la prima parola : ");
+        String parola= scanner.nextLine();
+
+
+
+
+    }
+}
